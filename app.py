4a60cfc21c8b7fae055d737c001d73a499297746
orangain <orangain@gmail.com>
2014-09-30 14:52:10 +0000

    render_index/mypage is now in views

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import redis
 
 from flask import (
     Flask, request, redirect, session, url_for, flash, jsonify,
-    render_template, _app_ctx_stack, get_flashed_messages, Response
+    _app_ctx_stack, Response
 )
 from werkzeug.contrib.fixers import ProxyFix
 
@@ -12,144 +12,13 @@ import os
 import hashlib
 from datetime import datetime
 
+from views import render_index, render_mypage
+
+
 config = {}
 app = Flask(__name__, static_url_path='')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.secret_key = os.environ.get('ISU4_SESSION_SECRET', 'shirokane')
-
-
-def render_index():
-    pieces = []
-    pieces.append(u"""
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css">
-    <link rel="stylesheet" href="/stylesheets/bootflat.min.css">
-    <link rel="stylesheet" href="/stylesheets/isucon-bank.css">
-    <title>isucon4</title>
-  </head>
-  <body>
-    <div class="container">
-      <h1 id="topbar">
-        <a href="/"><img src="/images/isucon-bank.png" alt="いすこん銀行 オンラインバンキングサービス"></a>
-      </h1>
-<div id="be-careful-phising" class="panel panel-danger">
-  <div class="panel-heading">
-    <span class="hikaru-mozi">偽画面にご注意ください！</span>
-  </div>
-  <div class="panel-body">
-    <p>偽のログイン画面を表示しお客様の情報を盗み取ろうとする犯罪が多発しています。</p>
-    <p>ログイン直後にダウンロード中や、見知らぬウィンドウが開いた場合、<br>すでにウィルスに感染している場合がございます。即座に取引を中止してください。</p>
-    <p>また、残高照会のみなど、必要のない場面で乱数表の入力を求められても、<br>絶対に入力しないでください。</p>
-  </div>
-</div>
-
-<div class="page-header">
-  <h1>ログイン</h1>
-</div>
-""")
-
-    messages = get_flashed_messages()
-    if messages:
-        pieces.append(u'<div id="notice-message" class="alert alert-danger" role="alert">')
-        pieces.append(messages[0])
-        pieces.append(u'</div>')
-
-    pieces.append(u"""
-<div class="container">
-  <form class="form-horizontal" role="form" action="/login" method="POST">
-    <div class="form-group">
-      <label for="input-username" class="col-sm-3 control-label">お客様ご契約ID</label>
-      <div class="col-sm-9">
-        <input id="input-username" type="text" class="form-control" placeholder="半角英数字" name="login">
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="input-password" class="col-sm-3 control-label">パスワード</label>
-      <div class="col-sm-9">
-        <input type="password" class="form-control" id="input-password" name="password" placeholder="半角英数字・記号（２文字以上）">
-      </div>
-    </div>
-    <div class="form-group">
-      <div class="col-sm-offset-3 col-sm-9">
-        <button type="submit" class="btn btn-primary btn-lg btn-block">ログイン</button>
-      </div>
-    </div>
-  </form>
-</div>
-
-    </div>
-
-  </body>
-</html>
-""")
-    return u''.join(pieces)
-
-
-def render_mypage():
-    return u''.join([u"""
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css">
-    <link rel="stylesheet" href="/stylesheets/bootflat.min.css">
-    <link rel="stylesheet" href="/stylesheets/isucon-bank.css">
-    <title>isucon4</title>
-  </head>
-  <body>
-    <div class="container">
-      <h1 id="topbar">
-        <a href="/"><img src="/images/isucon-bank.png" alt="いすこん銀行 オンラインバンキングサービス"></a>
-      </h1>
-<div class="alert alert-success" role="alert">
-  ログインに成功しました。<br>
-  未読のお知らせが０件、残っています。
-</div>
-
-<dl class="dl-horizontal">
-  <dt>前回ログイン</dt>
-  <dd id="last-logined-at">""", str(session['last_login_at']), u"""</dd>
-  <dt>最終ログインIPアドレス</dt>
-  <dd id="last-logined-ip">""", str(session['last_login_ip']), u"""</dd>
-</dl>
-
-<div class="panel panel-default">
-  <div class="panel-heading">
-    お客様ご契約ID：""", str(session['login']), u""" 様の代表口座
-  </div>
-  <div class="panel-body">
-    <div class="row">
-      <div class="col-sm-4">
-        普通預金<br>
-        <small>東京支店　1111111111</small><br>
-      </div>
-      <div class="col-sm-4">
-        <p id="zandaka" class="text-right">
-          ―――円
-        </p>
-      </div>
-
-      <div class="col-sm-4">
-        <p>
-          <a class="btn btn-success btn-block">入出金明細を表示</a>
-          <a class="btn btn-default btn-block">振込・振替はこちらから</a>
-        </p>
-      </div>
-
-      <div class="col-sm-12">
-        <a class="btn btn-link btn-block">定期預金・住宅ローンのお申込みはこちら</a>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-
-  </body>
-</html>
-"""])
 
 
 def load_config():
@@ -341,7 +210,7 @@ def get_ban_report():
 @app.route('/')
 def index():
     #return render_template('index.html')
-    return Response(render_index().encode('utf-8'))
+    return Response(render_index())
 
 
 @app.route('/login', methods=['POST'])
@@ -367,7 +236,7 @@ def login():
 def mypage():
     if session.get('user_id'):
         #return render_template('mypage.html', session=session)
-        return Response(render_mypage().encode('utf-8'))
+        return Response(render_mypage())
     else:
         flash('You must be logged in')
         return redirect(url_for('index'))
---
+++ views.py
@@ -0,0 +1,137 @@
+# coding: utf-8
+
+from flask import session, get_flashed_messages
+
+
+def render_index():
+    pieces = []
+    pieces.append("""
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="/stylesheets/bootflat.min.css">
+    <link rel="stylesheet" href="/stylesheets/isucon-bank.css">
+    <title>isucon4</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 id="topbar">
+        <a href="/"><img src="/images/isucon-bank.png" alt="いすこん銀行 オンラインバンキングサービス"></a>
+      </h1>
+<div id="be-careful-phising" class="panel panel-danger">
+  <div class="panel-heading">
+    <span class="hikaru-mozi">偽画面にご注意ください！</span>
+  </div>
+  <div class="panel-body">
+    <p>偽のログイン画面を表示しお客様の情報を盗み取ろうとする犯罪が多発しています。</p>
+    <p>ログイン直後にダウンロード中や、見知らぬウィンドウが開いた場合、<br>すでにウィルスに感染している場合がございます。即座に取引を中止してください。</p>
+    <p>また、残高照会のみなど、必要のない場面で乱数表の入力を求められても、<br>絶対に入力しないでください。</p>
+  </div>
+</div>
+
+<div class="page-header">
+  <h1>ログイン</h1>
+</div>
+""")
+
+    messages = get_flashed_messages()
+    if messages:
+        pieces.append('<div id="notice-message" class="alert alert-danger" role="alert">')
+        pieces.append(messages[0].encode('utf-8'))
+        pieces.append('</div>')
+
+    pieces.append("""
+<div class="container">
+  <form class="form-horizontal" role="form" action="/login" method="POST">
+    <div class="form-group">
+      <label for="input-username" class="col-sm-3 control-label">お客様ご契約ID</label>
+      <div class="col-sm-9">
+        <input id="input-username" type="text" class="form-control" placeholder="半角英数字" name="login">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="input-password" class="col-sm-3 control-label">パスワード</label>
+      <div class="col-sm-9">
+        <input type="password" class="form-control" id="input-password" name="password" placeholder="半角英数字・記号（２文字以上）">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-3 col-sm-9">
+        <button type="submit" class="btn btn-primary btn-lg btn-block">ログイン</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+    </div>
+
+  </body>
+</html>
+""")
+    return ''.join(pieces)
+
+
+def render_mypage():
+    return ''.join(["""
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="/stylesheets/bootflat.min.css">
+    <link rel="stylesheet" href="/stylesheets/isucon-bank.css">
+    <title>isucon4</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 id="topbar">
+        <a href="/"><img src="/images/isucon-bank.png" alt="いすこん銀行 オンラインバンキングサービス"></a>
+      </h1>
+<div class="alert alert-success" role="alert">
+  ログインに成功しました。<br>
+  未読のお知らせが０件、残っています。
+</div>
+
+<dl class="dl-horizontal">
+  <dt>前回ログイン</dt>
+  <dd id="last-logined-at">""", str(session['last_login_at']), """</dd>
+  <dt>最終ログインIPアドレス</dt>
+  <dd id="last-logined-ip">""", str(session['last_login_ip']), """</dd>
+</dl>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    お客様ご契約ID：""", session['login'].encode('utf-8'), """ 様の代表口座
+  </div>
+  <div class="panel-body">
+    <div class="row">
+      <div class="col-sm-4">
+        普通預金<br>
+        <small>東京支店　1111111111</small><br>
+      </div>
+      <div class="col-sm-4">
+        <p id="zandaka" class="text-right">
+          ―――円
+        </p>
+      </div>
+
+      <div class="col-sm-4">
+        <p>
+          <a class="btn btn-success btn-block">入出金明細を表示</a>
+          <a class="btn btn-default btn-block">振込・振替はこちらから</a>
+        </p>
+      </div>
+
+      <div class="col-sm-12">
+        <a class="btn btn-link btn-block">定期預金・住宅ローンのお申込みはこちら</a>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+
+  </body>
+</html>
+"""])
