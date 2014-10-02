5ebcb44e5cb3d0f141fa6be074bd53e0ca31d063
orangain <orangain@gmail.com>
2014-10-02 14:08:46 +0000

    Use tcp port for varnish

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
-#bind = '0.0.0.0:8080'
-bind = 'unix:/tmp/gunicorn.sock'
+bind = '0.0.0.0:8080'
+#bind = 'unix:/tmp/gunicorn.sock'
 workers = 10
 keepalive = 60
 worker_class = "meinheld.gmeinheld.MeinheldWorker"
